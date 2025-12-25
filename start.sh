a23b6ef039b04cf9c5919eb79ce4627f32e67551
WuShFeng <1481542813@qq.com>
2025-12-26 06:29:50 +0800

    fix: add openbox

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 Xorg $DISPLAY -config /etc/X11/xorg.conf.d/10-headless.conf \
     -nolisten tcp -background none \
     > /tmp/xorg.log 2>&1 &
-
+openbox \
+    > /tmp/openbox.log 2>&1 &
 # weston \
 #     --backend=x11-backend.so \
 #     --xwayland \
@@ -14,7 +15,7 @@ Xorg $DISPLAY -config /etc/X11/xorg.conf.d/10-headless.conf \
 #     -AlwaysShared \
 #     -rfbport 5900 \
 #     > /tmp/xvnc.log 2>&1 &
-x11vnc -display $DISPLAY -nopw -rfbport 5900 -xkb -forever > /tmp/x11vnc.log 2>&1 &
+x11vnc -display $DISPLAY -nopw -rfbport 5900 -forever -shared > /tmp/x11vnc.log 2>&1 &
 websockify --web /usr/share/novnc \
     6080 localhost:5900 \
     > /tmp/novnc.log 2>&1 &
